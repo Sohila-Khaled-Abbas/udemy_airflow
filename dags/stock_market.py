40d7eae8c104006c3251c52ea887c72c4898637d
Sohila <agro.sohila.k.abbas@gmail.com>
2026-04-10 21:45:38 +0200

    feat: add stock market DAG with API sensor and disable example DAGs in docker-compose

--- dags/stock_market.py
+++ dags/stock_market.py
@@ -1,4 +1,6 @@
 from airflow.decorators import dag, task
+from airflow.sensors.base import PokeReturnValue
+from airflow.hooks.base import BaseHook
 from datetime import datetime
 
 @dag (
@@ -7,10 +9,21 @@ from datetime import datetime
     catchup=False,
     tags = ['stock_market']
 )
+
 def stock_market():
     
-    @task.sensor(poke_interval=30, timeout=600)
-    def check_market_open():
-        pass
+    @task.sensor(poke_interval=30, timeout=300, mode='poke')
+    def is_api_available() -> PokeReturnValue:
+        import requests
+        
+        api = BaseHook.get_connection('stock_api')
+        url = f"{api.host}{api.extra_dejson['endpoint']}"
+        print(url)
+        response = requests.get(url, headers=api.extra_dejson['headers'])
+        condition = response.json()['finance']['result'] is None
+        return PokeReturnValue(is_done=condition, xcom_value=url)
 
+    is_api_available()
+    
+    
 stock_market()
